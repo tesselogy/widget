659efb610686446b616ea88181b6c8488d26bed2
ELEMIAN <elemiansds@gmail.com>
2020-03-23 16:30:47 +0300

    upload

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,12 @@ router.get('/', function(req, res) {
     var User = new ForgeSDK.UserProfileApi;
     User.getUserProfile(oAuth,req.session.credentials).then(function(profile){
       //console.log(profile);
-      res.render('user', {title: 'Айбим', menuconst: {'Хабы': '/hubs','Проекты': '/projects','Претензии': '/issues'}, firstName: profile.body.firstName, lastName: profile.body.lastName, img: profile.body.profileImages.sizeX120});
+      res.render('user', {title: 'Абсолют', menuconst: {'Хабы': '/hubs','Проекты': '/projects','Претензии': '/issues'}, firstName: profile.body.firstName, lastName: profile.body.lastName, img: profile.body.profileImages.sizeX120});
     },function (err){
       console.log(err);
-      res.render('index', { title: 'Айбим', menuconst: {'Хабы': '/hubs','Проекты': '/projects','Претензии': '/issues'}, url4adsk: oAuth.oAuth2ThreeLegged.generateAuthUrl()});
+      res.render('index', { title: 'Абсолют', menuconst: {'Хабы': '/hubs','Проекты': '/projects','Претензии': '/issues'}, url4adsk: oAuth.oAuth2ThreeLegged.generateAuthUrl()});
     })
-  } else {res.render('index', { title: 'Айбим', menuconst: {'Хабы': '/hubs','Проекты': '/projects','Претензии': '/issues'}, url4adsk: oAuth.oAuth2ThreeLegged.generateAuthUrl()});}
+  } else {res.render('index', { title: 'Абсолют', menuconst: {'Хабы': '/hubs','Проекты': '/projects','Претензии': '/issues'}, url4adsk: oAuth.oAuth2ThreeLegged.generateAuthUrl()});}
 });
 
 module.exports = router;
